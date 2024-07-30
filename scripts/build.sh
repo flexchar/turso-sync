27692f8143cc3171955d49ef92093c30ab321b86
Lukas <git@lukasnotes.dk>
2024-07-30 13:04:27 +0200

    build for servers and macs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+# Get the current timestamp
 now=$(date +%s)
+
+# Define tags
 tag="ghcr.io/flexchar/turso-sync:$now"
 latest="ghcr.io/flexchar/turso-sync:latest"
-echo "building image for tag: $tag"
-docker build --platform=linux/amd64 -t $tag -t $latest .
-docker push $tag
+
+# Print message about building image
+echo "Building multi-platform image for tags: $tag and $latest"
+
+# Create and use a new builder instance
+builder_name=$(docker buildx create --use)
+
+# Check if the buildx command was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to create and use the buildx builder instance"
+  exit 1
+fi
+
+# Build and push the multi-platform image
+docker buildx build --platform linux/amd64,linux/arm64 -t $tag -t $latest --push .
+
+# Check if the buildx command was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to build and push the multi-platform image"
+  docker buildx rm $builder_name
+  exit 1
+fi
+
+# Remove the builder instance
+docker buildx rm $builder_name
+
+echo "Multi-platform image built and pushed successfully with tags: $tag and $latest"
